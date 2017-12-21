a0490d3646a39f9de53b3a0d055b61597a39c81a
mwatson <mattwat@gmail.com>
2017-12-21 16:51:27 -0500

    extract js, add animation

--- assets/js/keycodes.js
+++ assets/js/keycodes.js
@@ -1,3 +1,13 @@
-window.addEventListener('keyDown', function() {
-	console.log("asd");
+window.addEventListener('keydown', function(e) {
+  keyDivs = document.querySelectorAll('.the-key');
+  for (let d of keyDivs) { d.innerText = e.key; }
+  keycodeDivs = document.querySelectorAll('.the-keycode');
+  for (let d of keycodeDivs) { d.innerText = e.keyCode; }
+
+	document.querySelector('.animator').style.visibility = 'visible';
+  document.querySelector('.animator').classList.toggle('pressed');
+	setTimeout(function() {
+  	document.querySelector('.animator').style.visibility = 'hidden';
+  	document.querySelector('.animator').classList.toggle('pressed');
+  }, 500);
 });
